afbc3278e55352376284a05366973a7225afb7d6
nayaba <nabila.ayaba@gmail.com>
2026-06-29 09:40:59 +0300

    Create initial html structure

---
+++ app.js
@@ -0,0 +1,15 @@
+// create an array of images (for SRC)
+// create an array of elements
+// create a start button (starts timer if using one)
+
+/*-------------------------------- Constants --------------------------------*/
+
+/*-------------------------------- Variables --------------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+const cardsEls = document.querySelectorAll('.card')
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+/*-------------------------------- Functions --------------------------------*/
